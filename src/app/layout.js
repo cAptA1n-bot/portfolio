2dd929938ea8be259ad3c741cca8c1a6b930a828
Prince Yadav <prince1bot001@gmail.com>
2026-06-19 10:53:53 +0530

    feat: star background

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import ShootingStarsAndStarsBackgroundDemo from "@/components/shooting-stars-and-stars-background-demo";
 
 export const metadata = {
   title: "Create Next App",
@@ -20,9 +10,17 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className="dark"
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="relative min-h-screen bg-neutral-950 text-foreground antialiased">
+        <div className="fixed inset-0 z-0 pointer-events-none">
+          <ShootingStarsAndStarsBackgroundDemo/>
+        </div>
+        
+        <div className="relative z-10">
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
